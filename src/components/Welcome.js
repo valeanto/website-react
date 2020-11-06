ad6b9c45f923bd08ea6bc5dbe8b48920cd20dcd3
Valentina Antollini <antolliniv@hotmail.com>
2020-11-06 16:27:26 +0100

    welcome page content

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,15 +1,105 @@
-import React from 'react'
+import React from "react";
 export default function Welcome() {
-    return (
-        <>   
-        <div className='app-grid-color'>
-            <section className='text-center'>
-            <h5>Online e-book</h5>
-            <h1>Best design practices in one place</h1>
-            <p>A free resource that will help you understand the design process and improve the quality of your work</p>
-            <button className='button'>Join as contributor</button>
+  return (
+    <>
+      <div className="app-grid">
+        <section className="section-textcenter background-color">
+          <h5>Online e-book</h5>
+          <h1>Best design practices in one place</h1>
+          <p>
+            A free resource that will help you understand the design process and
+            improve the quality of your work
+          </p>
         </section>
-        </div>
-        </>
-    )
+        <section className="section-col3">
+          <div>
+            <h5>benefits</h5>
+            <h1>What you can get with our design e-book</h1>
+            <p>
+              A free resource that will help you understand the design process
+              and improve the quality of your work
+            </p>
+          </div>
+          <div>
+            <img src=""></img>
+            <h2> Best Practices</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+          <div>
+            <img src=""></img>
+            <h2> Processes and tools</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+          <div></div>
+          <div>
+            <img src=""></img>
+            <h2> Communication</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+          <div>
+            <img src=""></img>
+            <h2> Client success</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+        </section>
+        <section className="textcenter">
+          <h5>e-book</h5>
+          <h1>A modern means of publication</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore
+          </p>
+          <img src="" />
+        </section>
+        <section>
+          <h5>users</h5>
+          <h1>Reflection</h1>
+          <div className="user-card"></div>
+          <div className="user-card"></div>
+          <div className="user-card"></div>
+        </section>
+        <section className="textcenter">
+          <h5>contribute</h5>
+          <h1>become one of the curators</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore{" "}
+          </p>
+        </section>
+        <button className="button">Join as contributor</button>
+      </div>
+    </>
+  );
 }
